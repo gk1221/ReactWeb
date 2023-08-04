3137396ce54a7cac5ed9b8c97eae82302985acf7
gk1221 <good125628@gmail.com>
2023-08-04 18:23:49 +0800

    API fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,9 +50,37 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "56f48927";
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const query = "interstellar";
+
+  useEffect(function () {
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?&apikey=${KEY}&s=${query}`
+        );
+
+        if (!res.ok) throw new Error("Something went wrong here");
+
+        const data = await res.json();
+        setMovies(data.Search);
+        console.log(data.Search);
+        setIsLoading(false);
+      } catch (err) {
+        console.error(err.message);
+        setError(err.message);
+      }
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <>
       <NavBar movies={movies}>
@@ -61,9 +89,15 @@ export default function App() {
       </NavBar>
       <Main movies={movies}>
         <Box>
-          <MovieList movies={movies} />
+          {error ? (
+            <ErrorMessage message={error} />
+          ) : isLoading ? (
+            <Loader />
+          ) : (
+            <MovieList movies={movies} />
+          )}
         </Box>
-        {/* <WatchedBox /> */}
+
         <Box>
           <>
             <WatchedSummary watched={watched} />
@@ -72,6 +106,19 @@ export default function App() {
         </Box>
       </Main>
     </>
+  );
+}
+
+function Loader() {
+  return <p className="loader">Just Loading...</p>;
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <p className="error">
+      <span>🚧</span>
+      {message}
+    </p>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} defaultRating={2} />
-    <StarRating maxRating={3} messages={["OK", "GOOD", "SAD"]} color="blue" />
+    <App />
+    {/* <StarRating maxRating={5} defaultRating={2} />
+    <StarRating maxRating={3} messages={["OK", "GOOD", "SAD"]} color="blue" /> */}
   </React.StrictMode>
 );
